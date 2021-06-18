3e13a7f2d2afeb15b3e74fc5bd3eaff340bda089
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-06-18 18:17:21 -0300

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -20,5 +20,6 @@ let handler  = async (m, { conn, args }) => {
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+handler.group = true
 
 module.exports = handler

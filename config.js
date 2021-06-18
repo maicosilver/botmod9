3b94ab3a8b1f5ae5ae24a24438aa90fcdaef816f
maicosilver <77900033+maicosilver@users.noreply.github.com>
2021-06-18 17:49:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['558896724995','994406932013','554784215225'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'nokia'
+global.author = '9672-4995'
 
 global.multiplier = 69 // The higher, The harder levelup
 
